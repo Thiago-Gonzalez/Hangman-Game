5ec79f0a4bc45a1d054bb947901a21504dfadcc7
Thiago González <thiagogonzalez2000@gmail.com>
2021-06-28 17:10:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import ASCII
 
 #Creating a words list
 words_list = []
+
 #Ask user: english or portuguese words
 language = input('What language do you choose? Type "br" for portuguese or "en" for english.\n')
 if language == "br":
@@ -17,8 +18,8 @@ else:
 os.system('clear') or None
 
 #Game loop. User will still playing while his answer is yes
-want_play = "yes"
-while want_play == "yes":
+want_play = True
+while want_play:
 
   #Randomly choose a word from the words_list and assign it to a variable called chosen_word
   chosen_word = random.choice(words_list)
@@ -28,10 +29,17 @@ while want_play == "yes":
 
   #Creates a null string
   word = ""
+  
+  #Creates a letter list
+  letters_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç', "ã", "á", "â", "é", "ê", "í", "ó", "õ", "ô", "ú"]
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç']
 
   #Fills the word_letters_blank_list, with same chosen_word's lenght, with blank spaces ("_")
-  for _ in range(0, len(chosen_word)):
-    blank_word_list.append("_ ")
+  for char in chosen_word:
+    if char not in letters_list:
+      blank_word_list += char
+    else:
+      blank_word_list += "_"
 
   #Creates the number of lifes the hanged man has
   lifes = 6
@@ -44,13 +52,10 @@ while want_play == "yes":
   #Prints
   print(ASCII.hangman)
   print(ASCII.hangedman_body_parts[0])
-  print(word)
+  print(' '.join(blank_word_list))
 
   #Creates a condition to end loop
   end_of_the_game = False
-
-  #Creates a letter list
-  letters_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç', '-', 'á', 'ã', 'â', 'é', 'ê', 'í', 'ó','õ', 'ô', 'ú']
 
 
   #Loops until the game is not ended
@@ -58,7 +63,7 @@ while want_play == "yes":
 
     #Allows the user to know all the letters he can still try to guess
     #Transforms letters list into a string
-    letters_print = ' '.join(letters_list)
+    letters_print = ' '.join(alphabet)
     #Prints letters user can still use
     print('Letters you still can use: ')
     print(letters_print)
@@ -67,21 +72,32 @@ while want_play == "yes":
     guess = input("\nGuess a letter: ").lower()
 
     #Checks if user has already guessed an especific letter
-    while guess not in letters_list:
+    while guess not in alphabet:
       print("You've already chosen this letter, please choose another. ")
       guess = input("\nGuess a letter: ").lower()
 
     #Letters already guessed
-    for letter in letters_list:
+    for letter in alphabet:
       if guess == letter:
-        letters_list.remove(letter)
+        alphabet.remove(letter)
       
   
     #Checks if the guessed letter (guess) is one of the letters in the chosen_word
-    for letter in range(0, len(blank_word_list)):
+    for pos in range(0, len(chosen_word)):
       #If the guessed letter is one of the letters in the chosen_word, replaces the corresponding space of the letter
-      if guess == chosen_word[letter]:
-        blank_word_list[letter] = guess
+      if guess == chosen_word[pos]:
+        blank_word_list[pos] = guess
+      if (chosen_word[pos] == "á" or chosen_word[pos] == "ã" or chosen_word[pos] == "â") and guess == "a":
+        blank_word_list[pos] = chosen_word[pos]
+      elif (chosen_word[pos] == "ó" or chosen_word[pos] == "õ" or chosen_word[pos] == "ô") and guess == "o":
+        blank_word_list[pos] = chosen_word[pos]
+      elif chosen_word[pos] == "í" and guess == "i":
+        blank_word_list[pos] = chosen_word[pos]
+      elif chosen_word[pos] == "ú" and guess == "u":
+        blank_word_list[pos] = chosen_word[pos]
+      elif (chosen_word[pos] == "é" or chosen_word[pos] == "ê") and guess == "e":
+        blank_word_list[pos] = chosen_word[pos]
+      
   
     #Checks if the guessed letter (guess) is one of the letters in the chosen_word
     #If it is not, lose a life
@@ -116,7 +132,7 @@ while want_play == "yes":
       print(ASCII.hangedman_body_parts[5])
     else:
       print(ASCII.hangedman_body_parts[6])
-    print(word)
+    print(' '.join(blank_word_list))
   
   
   #Checks if all letters has been already guessed or if lost all lifes
@@ -127,5 +143,7 @@ while want_play == "yes":
     print("You won!")
 
   #Ask user if still wants to play
-  want_play = input('Do you still want to play? Type "yes" or "no".\n')
+  play = input('Do you still want to play? Type "yes" or "no".\n')
+  if play == "no":
+    want_play = False
   os.system('clear') or None
